f525e224fe8562419c7509a708a938ac283822af
Ceciliapradob <ceciliapradob0799@gmail.com>
2024-02-29 13:35:11 -0300

    Culminado

--- src/components/Experiencia/ExperienciaLaboral.jsx
+++ src/components/Experiencia/ExperienciaLaboral.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import styles from "./ExperienciaLaboral.module.css"; // Asegúrate de importar tus estilos CSS
-import frontEnd from '/assets/certificate/frontend.png'; 
-import diseñoWeb from '/assets/certificate/diseñoweb.png'; 
-import argPrograma from '/assets/certificate/argprograma.png'; 
+import frontEnd from '/assets/certificate/frontend.png';
+import diseñoWeb from '/assets/certificate/diseñoweb.png';
+import argPrograma from '/assets/certificate/argprograma.png';
 
 const ExperienciaLaboral = () => {
   // Puedes agregar más experiencias según sea necesario
   const experiencias = [
     {
       id: 1,
-      certificado: frontEnd,
-      puesto: "Diplomatura frontEnd developer - UTN",
-      fecha: "Ago 2022 - Dic 2022",
-      descripcion: "Curso intenviso en desarrollo front end",
-      linkedin: "https://www.linkedin.com/in/cecilia-prado-29601521b/",
-    },
-    {
-      id: 2,
       certificado: diseñoWeb,
       puesto: "Docente en desarrollo web - Argentina Programa - UTN",
       fecha: "Feb 2023 - Actualmente",
@@ -24,18 +16,26 @@ const ExperienciaLaboral = () => {
       linkedin: "https://www.linkedin.com/in/cecilia-prado-29601521b/",
     },
     {
-      id: 3,
+      id: 2,
       certificado: argPrograma,
       puesto: "Docente en React Js - Arg Programa - UTN",
       fecha: "Feb 2023 - Actualmente",
       descripcion: "Dictado de cursos aplicando React Js.",
       linkedin: "https://www.linkedin.com/in/cecilia-prado-29601521b/",
     },
+    {
+      id: 3,
+      certificado: frontEnd,
+      puesto: "Diplomatura frontEnd developer - UTN",
+      fecha: "Ago 2022 - Dic 2022",
+      descripcion: "Curso intenviso en desarrollo front end",
+      linkedin: "https://www.linkedin.com/in/cecilia-prado-29601521b/"
+    },
   ];
 
   return (
     <section className={styles.experienciaLaboral}>
-      <hr  className={styles.line}/>
+      <hr className={styles.line} />
       <h2>Experiencia</h2>
       <div className={styles.experienciasContainer}>
         {experiencias.map((experiencia) => (
